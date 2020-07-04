12092cafb509108c3def739ba1b90381e18d377b
mawidicris <62735452+mawidicris@users.noreply.github.com>
2020-07-04 00:18:10 -0500

    Más codigo para gamines

--- ProyectoFinal/gamines.cpp
+++ ProyectoFinal/gamines.cpp
@@ -2,5 +2,25 @@
 
 Gamines::Gamines()
 {
+    ancho=70;
+    alto=70;
+    Gamine= new QPixmap(":/gamin.png");
+    timer= new QTimer;
+    connect(timer,&QTimer::timeout,this,&Gamines::caida);
+    timer->start(200);
 
 }
+QRectF Gamines::boundingRect() const
+{
+     return QRectF(-ancho/2,-alto/2,ancho,alto);
+}
+void Gamines::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->drawPixmap(-ancho/2,-alto/2,*Gamine,0,0,ancho,alto);
+}
+
+void Gamines::caida()
+{
+   setPos(x(),y()+15);
+}
+
--- ProyectoFinal/gamines.h
+++ ProyectoFinal/gamines.h
@@ -1,11 +1,26 @@
 #ifndef GAMINES_H
 #define GAMINES_H
 
-
-class Gamines
+#include <QObject>
+#include <QGraphicsItem>
+#include <QPainter>
+#include <QRectF>
+#include <QTimer>
+class Gamines : public QObject,public QGraphicsItem
 {
 public:
     Gamines();
+    explicit Gamines(QObject *parent = nullptr);
+    QPixmap *Gamine;
+    QTimer *timer;
+
+    float ancho,alto;
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+
+signals:
+public slots:
+void caida();
 };
 
 #endif // GAMINES_H
